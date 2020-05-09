8c469d8c08360d7473584137a418cecd3672fcda
Cyan Wang <Tianyu@maomaoqiu.local>
2020-05-09 11:01:21 +0800

    spa

---
+++ src/views/aboutus/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="app-container">
+    
+    <div>致命的紫罗兰</div>
+
+  </div>
+</template>
+
+<style scoped>
+.line{
+  text-align: center;
+}
+</style>
+
---
+++ src/views/search/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container" id="app">
+    <h3 style="text-align:center">动物森友会鱼类价格查询</h3>
+
+    <div style="text-align:center">
+      <el-input id="search-word" type="text" οnkeydοwn="jump(event);" placeholder />
+      <el-button @click="getData">搜索</el-button>
+    </div>
+  </div>
+</template>
